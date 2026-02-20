793bd45257f8992bd647c6bac263246058f16ff2
hkubus <38988733+hkubus@users.noreply.github.com>
2026-02-20 06:19:16 +0100

    fixes and change font to monospace

--- src/commands/fm.ts
+++ src/commands/fm.ts
@@ -6,14 +6,7 @@ import type { Agent } from "stanza";
 import type { Message } from "stanza/protocol";
 import { reply } from "../index.ts";
 import { getRecentTracks } from "../lastfm.ts";
-import { findSize } from "../splitText.ts";
-
-const isBright = (r: number, g: number, b: number) => {
-	const brightness = Math.sqrt(
-		0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b),
-	);
-	return brightness > 127.5;
-};
+import { findSize, isBright } from "../util.ts";
 
 export async function run(client: Agent, message: Message, db: Database) {
 	if (!message.from) return;
@@ -22,7 +15,10 @@ export async function run(client: Agent, message: Message, db: Database) {
 	const { lastfm } = db
 		.prepare("SELECT lastfm FROM users WHERE name = ?")
 		.get(nickname) as { lastfm: string };
-
+	if (!lastfm)
+		return reply(message, {
+			body: "you need to set your lastfm account with .setname <lastfm>",
+		});
 	const tracks = await getRecentTracks(lastfm, 2);
 	if (!tracks)
 		return reply(message, { body: "fmbot why you trying not to laugh" });
@@ -46,12 +42,12 @@ export async function run(client: Agent, message: Message, db: Database) {
 	const titleSize = findSize(ctx, tracks[0].title, 452);
 	const center = Math.round((512 + titleSize - 120) / 2);
 
-	ctx.font = `extrabold ${titleSize} Noto Sans`;
+	ctx.font = `extrabold ${titleSize} DejaVu Sans Mono`;
 	ctx.fillStyle = isBright(...dominantColor) ? "#000000bb" : "#ffffffbb";
 	ctx.fillText(tracks[0].title, 502, center);
-	ctx.font = "30px Noto Sans";
+	ctx.font = "30px DejaVu Sans Mono";
 	ctx.fillText(`${tracks[0].album}`, 502, center + 85);
-	ctx.font = "bold 30px Noto Sans";
+	ctx.font = "bold 30px DejaVu Sans Mono";
 	ctx.fillText(`${tracks[0].artist}`, 502, center + 45);
 	ctx.fillText(`Previous track`, 502, 392);
 	ctx.save();
@@ -61,29 +57,31 @@ export async function run(client: Agent, message: Message, db: Database) {
 	ctx.drawImage(coverImage, 30, 30, 452, 452);
 	ctx.restore();
 
+	// second track
 	ctx.beginPath();
 	ctx.roundRect(502, 402, 492, 80, 20);
-	ctx.fillStyle = "#00000020";
+	ctx.fillStyle = isBright(...dominantColor) ? "#00000020" : "#ffffff20";
 	ctx.fill();
-	ctx.closePath();
 	ctx.beginPath();
 	ctx.roundRect(512, 412, 60, 60, 10);
 	ctx.save();
 	ctx.clip();
-	const track = tracks.at(-1);
+	const track = tracks.at(1);
 	if (!track) return;
 	const secondCover = await loadImage(track.image);
-	ctx.drawImage(secondCover, 512, 412, 60, 60);
-	ctx.font = "bold 20px Noto Sans";
-	ctx.restore();
-	ctx.fillStyle = isBright(...dominantColor) ? "#000000bb" : "#ffffffbb";
 
+	ctx.drawImage(secondCover, 512, 412, 60, 60);
+	ctx.restore();
+
+	ctx.fillStyle = isBright(...dominantColor) ? "#000000bb" : "#ffffffbb";
+	ctx.font = "bold 30px DejaVu Sans Mono";
 	ctx.fillText(`${track.title}`, 582, 442);
-	ctx.font = "20px Noto Sans";
+	ctx.font = "20px DejaVu Sans Mono";
 	ctx.fillText(`${track.artist}`, 582, 467);
 
 	const blob = await canvas.convertToBlob({ mime: "image/png" });
-	const slot = await client.getUploadSlot("upload.gaydeer.ovh", {
+	// @ts-expect-error
+	const slot = await client.getUploadSlot(process.env.XMPP_UPLOAD_DOMAIN, {
 		name: "img.png",
 		size: blob.size,
 		type: "request",
--- src/util.ts
+++ src/util.ts
@@ -17,3 +17,10 @@ export function findSize(
 	}
 	return currentSize;
 }
+
+export function isBright(r: number, g: number, b: number) {
+	const brightness = Math.sqrt(
+		0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b),
+	);
+	return brightness > 127.5;
+}
